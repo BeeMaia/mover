fd5d78d04b2b3b38e46412a3716afb77985ef0e1
Andrea Maia <andrea.maia@beta80group.it>
2023-07-14 15:37:05 +0200

    add eventgrid event

---
+++ src/webapi/Mover.Shared/Models/BlobCreated.cs
@@ -0,0 +1,6 @@
+namespace Mover.Shared.Models;
+
+public class BlobCreated
+{
+    public string url { get; set; }
+}
--- src/webapi/modules/Mover.Modules.FitDecoder/FitDecoderDispatcher.cs
+++ src/webapi/modules/Mover.Modules.FitDecoder/FitDecoderDispatcher.cs
@@ -5,21 +5,23 @@ using Mover.Modules.FitDecoder.Shared.Events;
 using Mover.Shared;
 using Mover.Shared.Dispatchers;
 using Mover.Shared.Interfaces;
+using Mover.Shared.Models;
 
 namespace Mover.Modules.FitDecoder
 {
     public static class FitDecoderDispatcher
     {
-        [Topic(Constants.Dapr.MOVER_PUBSUB, nameof(DecodeFit))]
+        [Topic(Constants.Dapr.MOVER_PUBSUB, "decodefit")]
         public static Task<IResult> HandleDecodeFitAsync(DecodeFit command, ICommandHandler<DecodeFit> commandHandler, CancellationToken cancellationToken)
         {
             return CommandDispatcher.DispatchAsync(command, commandHandler, cancellationToken);
         }
 
-        [Topic(Constants.Dapr.MOVER_QUEUE, nameof(FitCreated))]
-        public static Task<IResult> HandleFitCreatedAsync(FitCreated @event, IEventHandler<FitCreated> eventHandler, CancellationToken cancellationToken)
+        [Topic(Constants.Dapr.MOVER_QUEUE, "fitcreated")]
+        public static Task<IResult> HandleFitCreatedAsync(EventGridEvent<BlobCreated> @event, IEventHandler<FitCreated> eventHandler, CancellationToken cancellationToken)
         {
-            return EventDispatcher.DispatchAsync(@event, eventHandler, cancellationToken);
+            var fileName = @event.data.url.Split('/').Last();
+            return EventDispatcher.DispatchAsync(new FitCreated(fileName), eventHandler, cancellationToken);
         }
     }
 }
