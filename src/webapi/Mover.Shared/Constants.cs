107ccd57b6b78591f502c2506246f9f07963ccc6
Andrea Maia <andrea.maia@beta80group.it>
2023-09-20 20:59:15 +0200

    add local dapr components

--- src/webapi/Mover.Shared/Constants.cs
+++ src/webapi/Mover.Shared/Constants.cs
@@ -4,7 +4,6 @@ public static class Constants
     public static class Dapr
     {
         public const string MOVER_PUBSUB = "mover-pubsub";
-        public const string MOVER_QUEUE = "mover-queue";
         public const string MOVER_FITBLOB = "mover-fitblob";
         public const string MOVER_GPXBLOB = "mover-gpxblob";
     }
