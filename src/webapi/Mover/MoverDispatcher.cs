9b123b4163fae8ee75ff8de755b6485fe53accc1
Andrea Maia <andrea.maia@beta80group.it>
2023-09-20 17:51:42 +0200

    upgrade to .Net 8

--- src/webapi/Mover/MoverDispatcher.cs
+++ src/webapi/Mover/MoverDispatcher.cs
@@ -12,13 +12,18 @@ namespace Mover
         [Topic(Constants.Dapr.MOVER_PUBSUB, "blobcreated")]
         public static Task<IResult> HandleBlobCreatedAsync(BlobCreated @event, IEventHandler<FitCreated> eventHandler, CancellationToken cancellationToken)
         {
+            if (@event.url is null)
+            {
+                throw new NotImplementedException();
+            }
+
             var fileName = @event.url.Split('/').Last();
 
             var extension = Path.GetExtension(fileName);
             if (extension.Equals(".fit"))
                 return EventDispatcher.DispatchAsync(new FitCreated(fileName), eventHandler, cancellationToken);
 
-            return Task.FromResult(Results.NoContent());
+            throw new NotImplementedException();
         }
     }
 }
--- src/webapi/modules/Mover.Modules.FitDecoder.Shared/Models/BlobCreated.cs
+++ src/webapi/modules/Mover.Modules.FitDecoder.Shared/Models/BlobCreated.cs
@@ -2,5 +2,5 @@ namespace Mover.Modules.FitDecoder.Shared.Models;
 
 public class BlobCreated
 {
-    public string url { get; set; }
+    public string? url { get; set; }
 }
