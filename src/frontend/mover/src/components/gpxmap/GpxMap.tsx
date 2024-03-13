ddff31e6cd7adfe23a302b8b81b38b439aba1890
Andrea Maia <andrea.maia@beta80group.it>
2024-03-13 22:05:18 +0100

    fix gpxmap

--- src/frontend/mover/src/components/gpxmap/GpxMap.tsx
+++ src/frontend/mover/src/components/gpxmap/GpxMap.tsx
@@ -8,8 +8,6 @@ interface GpxMapProps {
 
 const GpxMap: React.FC<GpxMapProps> = ({ positions }) => {
     const style = { height: "400px" };
-    const attribution =
-        '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
     return (
         <MapContainer
